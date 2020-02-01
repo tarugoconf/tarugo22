43154fc4f3163bac0058cbf90f26703aa65ee068
oscarotero <oom@oscarotero.com>
2020-02-01 15:00:05 +0100

    melloras de rendemento, axustes de estilo, nova cor azul secundaria

--- interaction.js
+++ interaction.js
@@ -8,3 +8,13 @@ button.addEventListener('click', e => menu.classList.toggle('is-open'));
 //Close the menu on click in the body
 document.body.addEventListener('click', e => menu.classList.remove('is-open'));
 navbar.addEventListener('click', e => e.stopPropagation());
+
+//Vimeo player
+const player = new Vimeo.Player('video', {
+    id: 384584744,
+    title: false,
+    byline: false,
+    portrait: false,
+    dnt: true,
+    color: 'EBFF00'
+});
