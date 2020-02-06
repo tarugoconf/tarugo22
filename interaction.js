e7c8bcfb5983dffce4eaf1fc0dd0c526e8cf72d6
oscarotero <oom@oscarotero.com>
2020-02-06 14:05:24 +0100

    iconos, cambio de tipo, etc

--- interaction.js
+++ interaction.js
@@ -1,19 +1,30 @@
+import Carousel from './vendor/@oom/carousel/carousel.js';
+import Player from './vendor/@oom/carousel/player.js';
+
 const button = document.querySelector('.navbar-menu-button');
-const menu = document.querySelector('.navbar-menu');
+const menu = document.querySelector('ul.navbar-menu');
 const navbar = document.querySelector('.navbar');
 
 //Open-close the menu
-button.addEventListener('click', e => menu.classList.toggle('is-open'));
+button.addEventListener('click', e => {
+    menu.classList.toggle('is-open');
+    e.stopPropagation();
+});
 
 //Close the menu on click in the body
 document.body.addEventListener('click', e => menu.classList.remove('is-open'));
-navbar.addEventListener('click', e => e.stopPropagation());
 
 //Vimeo player
-const player = new Vimeo.Player('video', {
-    title: false,
-    byline: false,
-    portrait: false,
-    dnt: true,
-    color: 'EBFF00'
-});
+document.querySelectorAll('.video-ratio').forEach(el =>
+    new Vimeo.Player('video', {
+        id: el.dataset.id,
+        title: false,
+        byline: false,
+        portrait: false,
+        dnt: true,
+        color: 'EBFF00'
+    })
+)
+
+//Carousel
+customElements.define('tarugo-carousel', Carousel);
---
+++ vendor/@oom/carousel/carousel.js
@@ -0,0 +1,133 @@
+export default class Carousel extends HTMLElement {
+    static get observedAttributes() {
+        return ['index'];
+    }
+
+    constructor() {
+        super();
+        this.scrollBehavior = 'smooth';
+    }
+
+    connectedCallback() {
+        //To calculate the offset of slides relative to the document
+        if (getStyleValue(this, 'position') === 'static') {
+            this.style.position = 'relative';
+        }
+
+        this.addEventListener('keydown', e => {
+            switch (e.keyCode) {
+                case 37: //left
+                    this.index -= 1;
+                    e.preventDefault();
+                    break;
+
+                case 39: //right
+                    this.index += 1;
+                    e.preventDefault();
+                    break;
+            }
+        });
+
+        checkScrollSupport(this);
+        checkA11y(this);
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'index') {
+            this.index = parseInt(newValue);
+        }
+    }
+
+    next(amount = 1) {
+        this.scrollFromLeft += this.clientWidth * amount;
+    }
+
+    prev(amount = 1) {
+        this.scrollFromLeft -= this.clientWidth * amount;
+    }
+
+    get index() {
+        const total = this.children.length - 1;
+        const slides = this.children;
+
+        for (let index = 0; index < total; index++) {
+            const slide = slides[index];
+            const scroll = Math.round(slide.offsetLeft - this.clientWidth / 2);
+
+            if (this.scrollLeft >= scroll && this.scrollLeft <= scroll + slide.clientWidth) {
+                return index;
+            }
+        }
+
+        return total;
+    }
+
+    set index(index) {
+        if (typeof index !== 'number' || Math.round(index) !== index) {
+            throw new Error('Invalid index value. It must be an integer');
+        }
+
+        const slides = this.children;
+        index = Math.min(Math.max(index, 0), slides.length - 1);
+        const slide = slides[index];
+        const scroll = Math.round(slide.offsetLeft - this.clientWidth / 2 + slide.clientWidth / 2);
+
+        this.scrollFromLeft = Math.max(0, scroll);
+    }
+
+    get scrollFromLeft() {
+        return this.scrollLeft;
+    }
+
+    set scrollFromLeft(scroll) {
+        try {
+            this.scroll({
+                left: scroll,
+                behavior: this.scrollBehavior
+            });
+        } catch (err) {
+            this.scrollLeft = scroll;
+        }
+    }
+
+    get scrollFromRight() {
+        return this.scrollWidth - this.clientWidth - this.scrollLeft;
+    }
+
+    set scrollFromRight(scroll) {
+        this.scrollFromLeft = this.scrollWidth - this.clientWidth - scroll;
+    }
+}
+
+function getStyleValue(el, name) {
+    const value = getComputedStyle(el)[name];
+
+    if (value && value.replace(/none/g, '').trim()) {
+        return value;
+    }
+}
+
+function checkScrollSupport(element) {
+    const support = 'scroll' in element && 'scrollBehavior' in element.style;
+
+    if (!support) {
+        console.info(
+            '@oom/carusel [compatibility]:',
+            'Missing smooth scrolling support. Consider using a polyfill like "smoothscroll-polyfill"'
+        );
+    }
+}
+
+function checkA11y(element) {
+    if (element.getAttribute('role') !== 'region') {
+        console.info('@oom/carusel [accesibility]:', 'Missing role="region" attribute in the carousel element');
+    }
+
+    if (!element.hasAttribute('aria-label')) {
+        console.info('@oom/carusel [accesibility]:', 'Missing aria-label attribute in the carousel element');
+    }
+
+    if (!element.hasAttribute('tabindex')) {
+        console.info('@oom/carusel [accesibility]:', 'Missing tabindex="0" attribute in the carousel element');
+    }
+}
