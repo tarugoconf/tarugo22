2e391fc26785ae7b857540897a1ace02b21bf849
oscarotero <oom@oscarotero.com>
2020-02-06 21:08:41 +0100

    melloras carousel

--- interaction.js
+++ interaction.js
@@ -33,12 +33,34 @@ document.querySelectorAll('tarugo-carousel').forEach(carousel => {
     const player = new Player(carousel);
     player.play();
 
-    carousel.parentElement.querySelector('.js-carousel-next').addEventListener('click', e => {
+    const next = carousel.parentElement.querySelector('.js-carousel-next');
+    const prev = carousel.parentElement.querySelector('.js-carousel-prev');
+
+    next.addEventListener('click', e => {
         player.stop();
         carousel.next();
     })
-    carousel.parentElement.querySelector('.js-carousel-prev').addEventListener('click', e => {
+    prev.addEventListener('click', e => {
         player.stop();
         carousel.prev();
     })
+
+    //Disable buttons
+    let isScrolling;
+
+    carousel.addEventListener(
+        'scroll',
+        event => {
+            clearTimeout(isScrolling);
+            isScrolling = setTimeout(showHideButtons, 100);
+        },
+        false
+    );
+
+    function showHideButtons() {
+        prev.disabled = carousel.scrollFromLeft < 5;
+        next.disabled = carousel.scrollFromRight < 5;
+    }
+
+    showHideButtons();
 })
