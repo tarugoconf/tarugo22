33c8f32a3460ca80b6973a035fea70e535b9d72b
Oscar Otero <oom@oscarotero.com>
2021-10-12 20:20:51 +0200

    basePath plugin

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import inline from "lume/plugins/inline.ts";
+import basePath from "lume/plugins/base_path.ts";
 import gpm from "https://deno.land/x/gpm@v0.3.0/mod.ts";
 
 const site = lume();
@@ -13,6 +14,7 @@ site
   .copy("vendor")
   .copy("interaction.js")
   .use(postcss({ includes: "_css" }))
+  .use(basePath())
   .use(inline());
 
 site.addEventListener("beforeBuild", () => gpm(["oom-components/carousel"]));
